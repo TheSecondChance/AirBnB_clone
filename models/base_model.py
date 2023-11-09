cd27a41bd75669aebd03bd441c5a661bead5838d
eyasu <mesakmegerem@gmail.com>
2023-11-08 16:43:00 -0800

    add arg and kwarg

--- models/base_model.py
+++ models/base_model.py
@@ -8,12 +8,21 @@ from datetime import datetime
 class BaseModel:
     """Base class for all hbnb models"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """instatntiat object"""
-        self.id = str(uuid.uuid4())
-
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        format = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs is not None and kwargs != {}:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, format))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
 
     def save(self):
         """updated current date time"""
