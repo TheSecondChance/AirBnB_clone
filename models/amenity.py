fde3e731e271a57e0ec43aa0ed40a195ed3127da
eyasu <mesakmegerem@gmail.com>
2023-11-11 14:13:54 -0800

    amenity.py

---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module Creates Amenity"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Class Amenity objects inheri BaseModel"""
+
+    name = ""
