163cd1ec4b2cf78c5019756dfb0851e69801b3f2
eyasu <mesakmegerem@gmail.com>
2023-11-11 05:21:53 -0800

    requi

--- models/base_model.py
+++ models/base_model.py
@@ -20,9 +20,12 @@ class BaseModel:
                     setattr(self, key, datetime.strptime(value, format))
                 else:
                     setattr(self, key, value)
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
+            storage.new(self)
+
         storage.new(self)
 
     def save(self):
