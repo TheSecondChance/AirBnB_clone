038b614b9a8b3820db5525c6873cd46ec4aef8bf
eyasu <mesakmegerem@gmail.com>
2023-11-11 14:29:07 -0800

    test_state.py

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Create Test"""
+import unittest
+from models.engine.file_storage import FileStorage
+
+
+class Test_state(unittest.TestCase):
+    """want to pass"""
+
+    pass
