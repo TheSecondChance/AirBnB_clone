086450c65ffc8d4c0f2d2048004e8f8fcc69a0c8
eyasu <mesakmegerem@gmail.com>
2023-11-12 01:26:55 -0800

    modify file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,6 +68,16 @@ class FileStorage:
         """Valid classe and references be"""
         from models.base_model import BaseModel
         from models.user import User
+        from models.amenity import Amenity
+        from models.city import City
+        from models.place import Place
+        from models.review import Review
+        from models.state import State
 
-        classe = {"BaseModel": BaseModel, "User": User}
+        classe = {
+                "BaseModel": BaseModel, "User": User, "Amenity": Amenity,
+                "City": City,
+                "Place": Place,
+                "Review": Review,
+                "State": State}
         return classe
